55ed46385233f2bcb8a22d8644c08eb097614200
danasilver <dsilver1221@gmail.com>
2013-12-17 19:54:31 -0500

    update readme and cleanup routes

--- README.md
+++ README.md
@@ -1,7 +1,21 @@
 # Middlebury Menu API
 An REST API for menus.middlebury.edu.
 
-_Updated 12/16/13_
+_Updated 12/17/13_
+
+## Endpoints
+
+API base url: `middmenuapi.herokuapp.com`.
+
+```
+/
+```
+Returns the current day's menu.
+
+```
+/yyyy-mm-dd
+```
+Returns the menu for `yyyy-mm-dd`.  
 
 ## Development
 
@@ -14,7 +28,3 @@ Run the server:
 ```sh
 $ node app.js
 ```
-
-## To do
-* Add database to cache menu
-* Add job to fetch menu daily
--- routes.js
+++ routes.js
@@ -29,85 +29,85 @@ exports.getDateEST = getDateEST;
 // and respond to a request
 function getAnyDateMenu(date, req, res) {
     var payload = {
-                'field_day_value[value][date]': date,
-                'view_name': 'menus_test',
-                'view_display_id': 'page',
-                'view_args': '',
-                'view_path': 'dining',
-                'view_base_path': 'dining',
-                'view_dom_id': '56a3964e9d683783129d622f9a79b171',
-                'pager_element': '0',
-                'ajax_html_ids[]': 'skip-link',
-                'ajax_html_ids[]': 'midd_search_query',
-                'ajax_html_ids[]': 'midd_search_submit',
-                'ajax_html_ids[]': 'midd_ajax_search_url',
-                'ajax_html_ids[]': 'midd_wordmark',
-                'ajax_html_ids[]': 'midd_navigation',
-                'ajax_html_ids[]': 'midd_content',
-                'ajax_html_ids[]': 'navigation',
-                'ajax_html_ids[]': 'nav_dining',
-                'ajax_html_ids[]': 'nav_retail',
-                'ajax_html_ids[]': 'nav_comment',
-                'ajax_html_ids[]': 'views-exposed-form-menus-test-page',
-                'ajax_html_ids[]': 'edit-field-day-value-wrapper',
-                'ajax_html_ids[]': 'edit-field-day-value-value-wrapper',
-                'ajax_html_ids[]': 'edit-field-day-value-value',
-                'ajax_html_ids[]': 'edit-field-day-value-value',
-                'ajax_html_ids[]': 'edit-field-day-value-value-datepicker-popup-0',
-                'ajax_html_ids[]': 'edit-submit-menus-test',
-                'ajax_html_ids[]': 'midd_footer',
-                'ajax_html_ids[]': 'midd_footer_panel',
-                'ajax_html_ids[]': 'ui-datepicker-div',
-                'ajax_page_state[theme]': 'middsatellite',
-                'ajax_page_state[theme_token]': '-sLphxibd9eikzNUq_vyU5IvCdJNRFnzwSkydIx0hYI',
-                'ajax_page_state[css][modules/system/system.base.css]': '1',
-                'ajax_page_state[css][modules/system/system.menus.css]': '1',
-                'ajax_page_state[css][modules/system/system.messages.css]': '1',
-                'ajax_page_state[css][modules/system/system.theme.css]': '1',
-                'ajax_page_state[css][misc/ui/jquery.ui.core.css]': '1',
-                'ajax_page_state[css][misc/ui/jquery.ui.theme.css]': '1',
-                'ajax_page_state[css][misc/ui/jquery.ui.datepicker.css]': '1',
-                'ajax_page_state[css][sites/all/modules/date/date_popup/themes/jquery.timeentry.css]': '1',
-                'ajax_page_state[css][modules/comment/comment.css]': '1',
-                'ajax_page_state[css][sites/all/modules/date/date_api/date.css]': '1',
-                'ajax_page_state[css][sites/all/modules/date/date_popup/themes/datepicker.1.7.css]': '1',
-                'ajax_page_state[css][modules/field/theme/field.css]': '1',
-                'ajax_page_state[css][modules/node/node.css]': '1',
-                'ajax_page_state[css][modules/search/search.css]': '1',
-                'ajax_page_state[css][modules/user/user.css]': '1',
-                'ajax_page_state[css][sites/all/modules/views/css/views.css]': '1',
-                'ajax_page_state[css][sites/all/modules/ctools/css/ctools.css]': '1',
-                'ajax_page_state[css][sites/all/themes/middsatellite/system.menus.css]': '1',
-                'ajax_page_state[css][sites/all/themes/middsatellite/system.messages.css]': '1',
-                'ajax_page_state[css][sites/all/themes/middsatellite/system.theme.css]': '1',
-                'ajax_page_state[css][sites/all/themes/middsatellite/css/styles.css]': '1',
-                'ajax_page_state[css][sites/all/themes/middsatellite/css/meal-menus.css]': '1',
-                'ajax_page_state[js][misc/jquery.js]': '1',
-                'ajax_page_state[js][misc/jquery.once.js]': '1',
-                'ajax_page_state[js][misc/drupal.js]': '1',
-                'ajax_page_state[js][misc/ui/jquery.ui.core.min.js]': '1',
-                'ajax_page_state[js][misc/jquery.cookie.js]': '1',
-                'ajax_page_state[js][misc/jquery.form.js]': '1',
-                'ajax_page_state[js][misc/ui/jquery.ui.datepicker.min.js]': '1',
-                'ajax_page_state[js][sites/all/modules/date/date_popup/jquery.timeentry.pack.js]': '1',
-                'ajax_page_state[js][misc/ajax.js]': '1',
-                'ajax_page_state[js][sites/all/modules/date/date_popup/date_popup.js]': '1',
-                'ajax_page_state[js][sites/all/modules/views/js/base.js]': '1',
-                'ajax_page_state[js][misc/progress.js]': '1',
-                'ajax_page_state[js][sites/all/modules/views/js/ajax_view.js]': '1'
-            }
-            request({
-                method: 'POST',
-                url: 'http://menus.middlebury.edu/views/ajax',
-                form: payload,
-                json: true
-            }, function(err, request_res, body) {
-                if (!err && request_res.statusCode == 200) {
-                    var menu = scraper.parseMenu(date, body[2].data);
+        'field_day_value[value][date]': date,
+        'view_name': 'menus_test',
+        'view_display_id': 'page',
+        'view_args': '',
+        'view_path': 'dining',
+        'view_base_path': 'dining',
+        'view_dom_id': '56a3964e9d683783129d622f9a79b171',
+        'pager_element': '0',
+        'ajax_html_ids[]': 'skip-link',
+        'ajax_html_ids[]': 'midd_search_query',
+        'ajax_html_ids[]': 'midd_search_submit',
+        'ajax_html_ids[]': 'midd_ajax_search_url',
+        'ajax_html_ids[]': 'midd_wordmark',
+        'ajax_html_ids[]': 'midd_navigation',
+        'ajax_html_ids[]': 'midd_content',
+        'ajax_html_ids[]': 'navigation',
+        'ajax_html_ids[]': 'nav_dining',
+        'ajax_html_ids[]': 'nav_retail',
+        'ajax_html_ids[]': 'nav_comment',
+        'ajax_html_ids[]': 'views-exposed-form-menus-test-page',
+        'ajax_html_ids[]': 'edit-field-day-value-wrapper',
+        'ajax_html_ids[]': 'edit-field-day-value-value-wrapper',
+        'ajax_html_ids[]': 'edit-field-day-value-value',
+        'ajax_html_ids[]': 'edit-field-day-value-value',
+        'ajax_html_ids[]': 'edit-field-day-value-value-datepicker-popup-0',
+        'ajax_html_ids[]': 'edit-submit-menus-test',
+        'ajax_html_ids[]': 'midd_footer',
+        'ajax_html_ids[]': 'midd_footer_panel',
+        'ajax_html_ids[]': 'ui-datepicker-div',
+        'ajax_page_state[theme]': 'middsatellite',
+        'ajax_page_state[theme_token]': '-sLphxibd9eikzNUq_vyU5IvCdJNRFnzwSkydIx0hYI',
+        'ajax_page_state[css][modules/system/system.base.css]': '1',
+        'ajax_page_state[css][modules/system/system.menus.css]': '1',
+        'ajax_page_state[css][modules/system/system.messages.css]': '1',
+        'ajax_page_state[css][modules/system/system.theme.css]': '1',
+        'ajax_page_state[css][misc/ui/jquery.ui.core.css]': '1',
+        'ajax_page_state[css][misc/ui/jquery.ui.theme.css]': '1',
+        'ajax_page_state[css][misc/ui/jquery.ui.datepicker.css]': '1',
+        'ajax_page_state[css][sites/all/modules/date/date_popup/themes/jquery.timeentry.css]': '1',
+        'ajax_page_state[css][modules/comment/comment.css]': '1',
+        'ajax_page_state[css][sites/all/modules/date/date_api/date.css]': '1',
+        'ajax_page_state[css][sites/all/modules/date/date_popup/themes/datepicker.1.7.css]': '1',
+        'ajax_page_state[css][modules/field/theme/field.css]': '1',
+        'ajax_page_state[css][modules/node/node.css]': '1',
+        'ajax_page_state[css][modules/search/search.css]': '1',
+        'ajax_page_state[css][modules/user/user.css]': '1',
+        'ajax_page_state[css][sites/all/modules/views/css/views.css]': '1',
+        'ajax_page_state[css][sites/all/modules/ctools/css/ctools.css]': '1',
+        'ajax_page_state[css][sites/all/themes/middsatellite/system.menus.css]': '1',
+        'ajax_page_state[css][sites/all/themes/middsatellite/system.messages.css]': '1',
+        'ajax_page_state[css][sites/all/themes/middsatellite/system.theme.css]': '1',
+        'ajax_page_state[css][sites/all/themes/middsatellite/css/styles.css]': '1',
+        'ajax_page_state[css][sites/all/themes/middsatellite/css/meal-menus.css]': '1',
+        'ajax_page_state[js][misc/jquery.js]': '1',
+        'ajax_page_state[js][misc/jquery.once.js]': '1',
+        'ajax_page_state[js][misc/drupal.js]': '1',
+        'ajax_page_state[js][misc/ui/jquery.ui.core.min.js]': '1',
+        'ajax_page_state[js][misc/jquery.cookie.js]': '1',
+        'ajax_page_state[js][misc/jquery.form.js]': '1',
+        'ajax_page_state[js][misc/ui/jquery.ui.datepicker.min.js]': '1',
+        'ajax_page_state[js][sites/all/modules/date/date_popup/jquery.timeentry.pack.js]': '1',
+        'ajax_page_state[js][misc/ajax.js]': '1',
+        'ajax_page_state[js][sites/all/modules/date/date_popup/date_popup.js]': '1',
+        'ajax_page_state[js][sites/all/modules/views/js/base.js]': '1',
+        'ajax_page_state[js][misc/progress.js]': '1',
+        'ajax_page_state[js][sites/all/modules/views/js/ajax_view.js]': '1'
+    }
+    request({
+        method: 'POST',
+        url: 'http://menus.middlebury.edu/views/ajax',
+        form: payload,
+        json: true
+    }, function(err, request_res, body) {
+        if (!err && request_res.statusCode == 200) {
+            var menu = scraper.parseMenu(date, body[2].data);
 
-                    res.send(menu);
-                }
-            });
+            res.send(menu);
+        }
+    });
 }
 
 exports.today = function(req, res) {
@@ -134,11 +134,12 @@ exports.findDate = function(req, res) {
             });
         }
         else {
+            // Future date
             getAnyDateMenu(requestDate, req, res);   
         }
     }
     else {
-        res.send(404, 'Menu not found for ' + requestDate + '.');
+        res.send(404, 'Menu not found for ' + requestDate + '.\n' + 'Proper format is `/yyyy-mm-dd`.');
     }
     
 }
