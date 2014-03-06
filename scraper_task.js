248cd8ee6edf7ee92b5e8257e21c3ee49b912bc5
danasilver <dsilver1221@gmail.com>
2014-03-06 00:50:02 -0500

    Update scraper_task to requst actual date instead of generic today.

--- scraper_task.js
+++ scraper_task.js
@@ -1,14 +1,19 @@
 #!/usr/bin/env node
 
 var request = require('request'),
+    moment = require('moment'),
     mongoose = require('mongoose'),
     scraper = require('./scraper'),
     routes = require('./routes'),
     Menu = require('./models/Menu');
 
+var requestURL = 'https://menus.middlebury.edu/dining?' +
+                 encodeURIComponent('field_day_value[value][date]') +
+                 '=' + moment(routes.getDateEST(new Date()), 'YYYY-MM-DD').format('dddd, MMMM D, YYYY').replace(/ /g, "%20");
+
 request({
     method: 'GET',
-    url: 'https://menus.middlebury.edu'
+    url: requestURL,
 }, function(err, res, body) {
     if (!err && res.statusCode == 200){
         var menu = scraper.parseMenu(routes.getDateEST(new Date()), body);
